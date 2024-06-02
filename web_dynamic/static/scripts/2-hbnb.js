f88ae7901d2254c907c07fab775fc0ccd147b64d
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-02 05:28:16 +0100

    Debugged task 3 successfully

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,26 +13,21 @@ $(document).ready(function () {
       }
   
       const amenityNames = Object.values(selectedAmenities);
+
       if (amenityNames.length > 0) {
-        $('.amenities h4').text(amenityNames.join(', '));
+        $('.amenities h4').text(amenityNames.sort().join(', '));
       } else {
         $('.amenities h4').html('&nbsp;');
       }
     });
   
     // Check the API status
-    $.get('http://0.0.0.0:5001/api/v1/status/')
-      .done(function (data) {
-        console.log('API response:', data);
+    $.getJSON('http://localhost:5001/api/v1/status/', (data) => {
         if (data.status === 'OK') {
           $('#api_status').addClass('available');
         } else {
           $('#api_status').removeClass('available');
         }
-      })
-      .fail(function (jqXHR, textStatus, errorThrown) {
-        console.error('Failed to fetch API status:', textStatus, errorThrown);
-        $('#api_status').removeClass('available');
-      });
+    });
   });
   
